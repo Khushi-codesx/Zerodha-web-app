cc5c9e48faeeb0aaff0e2257487a9341801970c8
khushi <ikhushisingh005@gmail.com>
2026-01-17 11:51:41 +0530

    Zerodha Clone Project

---
+++ src/Footer.js
@@ -0,0 +1,155 @@
+import React from 'react';
+function Footer() {
+    return ( 
+        <footer style={{backgroundColor:"rgb(250,250,250)"}}>
+      <div className='container border-top mt-5' >
+        <div className='row mt-5'>
+            <div className='col'>
+                <img src='media/logo.svg' style={{width:"50%"}}/>
+                <p> &copy; 2010 - 2025, Zerodha Broking Ltd.  All rights reserved.</p>
+            </div>
+             <div className='col'>
+                <p> Company </p>
+                
+               <a href=" " class="text-muted " 
+                    style={{textDecoration:"none",fontSize:"14px"}}>About</a> <br></br>
+               <a href=" " class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px",paddingBottom:"10px"}}> Philosophy</a><br></br>
+                <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px",paddingBottom:"10px"}}> Press & media </a> <br></br>   
+                 <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px",paddingBottom:"10px"}}> Careers </a>   <br></br> 
+                  <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Zerodha Cares (CSR) </a> <br></br>   
+                   <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>  Zerodha.tech </a>   <br></br> 
+                    <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>  Open source</a>  <br></br>
+                  
+
+ 
+             </div>
+              <div className='col'>
+                  <p>Support </p>
+                   
+                     <a href=" " class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Contact us</a>
+                     <br></br>
+                    <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Support portal</a> 
+                    <br></br>   
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> How to file a complaint? </a>  
+                      <br></br> 
+                    <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Status of your complaints </a>
+                     <br></br>   
+                   <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Bulletin </a> 
+                     <br></br> 
+                    <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Circular </a>
+                      <br></br>
+                       <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Z-Connect blog </a>
+                      <br></br>
+                       <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Downloads</a>
+                      <br></br>
+                     
+                       
+              </div>
+              <div className='col'>
+                  <p>Account </p>
+                  <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Open demat account</a>
+                  <br></br>
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Minor demat account</a>
+                  <br></br>
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>NRI demat account </a>
+                  <br></br>
+                      <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Commodity </a>
+                  <br></br>
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Dematerialisation</a>
+                  <br></br>
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Fund transfer </a>
+                  <br></br>
+                    <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>MTF </a>
+                  <br></br>
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Referral program  </a>
+                  <br></br>
+              </div>
+
+
+
+               <div className='col '>
+                  <p>Quick links </p>
+                    <a href='' 
+                    class="text-muted" style={{textDecoration:"none",fontSize:"14px"}}> 
+                    Upcoming IPOs</a>
+                  <br></br>
+                
+                     <a href='' class="text-muted " 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Brokerage charges </a>
+                  <br></br>
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Market holidays</a>
+                  <br></br>
+                    <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Economic calendar</a>
+                  <br></br>
+                    <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Calculators </a>
+                  <br></br>
+                     <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}> Markets</a>
+                  <br></br>
+                      <a href='' class="text-muted" 
+                    style={{textDecoration:"none",fontSize:"14px"}}>Sectors </a>
+                  <br></br>
+              </div>
+              <div className='mt-5 text-muted' style={{fontSize:"14px"}}>
+                <p>Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF
+                    </p>
+                    <p>
+                        Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances
+                    </p>
+                    <p>
+                        Investments in securities market are subject to market risks; read all the related documents carefully before investing.
+                    </p>
+                    <p>
+                        "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.
+                        <br>
+                        </br>
+                        <br></br>
+                        <p>*Customers availing insurance advisory services offered by Ditto (Tacterial Consulting Private Limited | IRDAI Registered Corporate Agent (Composite) License No CA0738) will not have access to the exchange investor grievance redressal forum, SEBI SCORES/ODR, or arbitration mechanism for such products.</p>
+                    </p>
+                    </div>
+                   
+                         <ul class="d-flex justify-content-center gap-4 mt-3 text-sm text-muted">
+                        <li class="d-inline">NSE </li>
+                        <li class="d-inline mr-3"> BSE </li>
+                          <li class="d-inline mr-3">   MCX  </li>
+                            <li class="d-inline mr-3"> Terms & conditions</li>
+                              <li class="d-inline mr-3">Policies & procedures </li> 
+                               <li class="d-inline mr-3">Privacy policy  </li>
+                                <li class="d-inline mr-3"> Disclosure </li>  
+                                <li class="d-inline mr-3"> For investor's attention</li>
+                                <li class="d-inline mr-3"> Investor charter </li> 
+                            
+                    </ul>
+
+        </div>
+      </div>
+      </footer>
+    );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HomePage from './home/HomePage';
+import Signup from "./signup/Signup";
+import AboutPage from "./about/AboutPage";
+import ProductPage from "./products/ProductsPage";
+import PricingPage from "./pricing/PricingPage";
+import SupportPage from "./support/SupportPage";
+import Navbar from './Navbar';
+import Footer from './Footer';
+import NotFound from './NotFound';
+
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='signup' element={<Signup/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/product' element={<ProductPage/>}/>
+          <Route path='/pricing' element={<PricingPage/>}/>
+          <Route path='/support' element={<SupportPage/>}/>
+            <Route path='*' element={<NotFound/>}/>
+     </Routes>
+     
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
